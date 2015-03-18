dfa3a90ef5f0f2cb0c1f6bfddea9935f0af0d15c
hunterladdwdd <dhunter.ladd@gmail.com>
2015-03-18 17:18:00 -0500

    Minor changes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Austinpetstylist
     config.active_support.escape_html_entities_in_json = true
 
     config.assets.enabled = true
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.generators do |g|
       g.orm :active_record
       g.template_engine :haml
