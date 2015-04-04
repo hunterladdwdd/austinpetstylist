abfed3f6abf9839afb784ff38134a77df27704be
hunterladdwdd <dhunter.ladd@gmail.com>
2015-04-04 16:59:28 -0500

    Minor change

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -3,6 +3,7 @@ class Testimonial < ActiveRecord::Base
   
   validates_presence_of :body
   validates_presence_of :client
+  validates_presence_of :user_id
 
   def preview
     if self.body.size < 300
